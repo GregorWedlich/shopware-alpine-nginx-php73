4abe216f8a673722b6ac9a448ebca773faf21bf7
Print Monty <g.wedlich@me.com>
2019-12-04 23:16:40 +0100

    Add plugins folders to host.
    Signed-off-by: Print Monty <g.wedlich@me.com>

--- conf/docker-entrypoint.sh
+++ conf/docker-entrypoint.sh
@@ -12,4 +12,20 @@ if [ -z "$(ls -A /var/www/html/themes)" ]; then
     chown -R nobody.nobody /var/www/html/themes/
 fi
 
+if [ -z "$(ls -A /var/www/html/engine/Shopware/Plugins)" ]; then
+    echo "Move plugin folder from shopware clean download"
+    cp -R /home/shopware_files/download/engine/Shopware/Plugins/* /var/www/html/engine/Shopware/Plugins/
+    
+    echo "Set permissions"
+    chown -R nobody.nobody /var/www/html/engine/Shopware/Plugins/
+fi
+
+if [ -z "$(ls -A /var/www/html/custom)" ]; then
+    echo "Move plugins folder SW >= 5.2 from shopware clean download"
+    cp -R /home/shopware_files/download/custom/* /var/www/html/custom/
+    
+    echo "Set permissions"
+    chown -R nobody.nobody /var/www/html/custom/
+fi
+
 exec "$@";
