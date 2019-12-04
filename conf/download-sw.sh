9b363b2fc90c0ca4a59322d204afc151d84c8c0e
Print Monty <g.wedlich@me.com>
2019-12-04 23:15:52 +0100

    Update to SW 5.6.3
    Signed-off-by: Print Monty <g.wedlich@me.com>

--- conf/download-sw.sh
+++ conf/download-sw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOWNLOAD_URL="https://releases.shopware.com/install_5.6.2_6cadc5c14bad4ea8839395461ea42dbc359e9666.zip"
+DOWNLOAD_URL="https://releases.shopware.com/install_5.6.3_fec7645a72a0393f8a39f48ddd6c27e138f44513.zip"
 
 wget -O /home/shopware_files/download/shop.zip $DOWNLOAD_URL
 
